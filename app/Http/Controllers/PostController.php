f44c08772a35c330d08b4b3725885eecc72286dd
Christopher Chirino <critijo@gmail.com>
2021-06-09 00:07:38 -0400

    First commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,105 @@
+<?php
+use Illuminate\Support\Facades\Redirect;
+namespace App\Http\Controllers;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return Inertia::render('List', ['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Form');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'title' => 'required',
+                'description' => 'required',
+                'content' => 'required',
+                'status' => 'required',
+
+            ]
+        );
+
+        Product::create($request->all());
+
+        return Redirect::route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $posts = Post::find($id);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return Inertia::render('EditForm', ['id' => $id]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $posts = Post::find($id);
+        $posts->update($request->all());
+        return Redirect::route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $posts = Post::find($id);
+        $posts->delete();
+        return Redirect::route('posts.index');
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title', 
+        'description', 
+        'content',
+        'status',
+        'user_id',
+        'category_id',
+
+    ];
+
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relacion Polimorfica de uno a uno
+
+    public function images (){
+        return $this->morphOne(Image::class, 'images');
+    }
+
+}
