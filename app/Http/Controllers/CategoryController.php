9a5a3b3b3b23866b084d14befb7fed31ad484ed3
Christopher Chirino <critijo@gmail.com>
2021-06-09 00:21:27 -0400

    CategoryController

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,103 @@
+<?php
+use Illuminate\Support\Facades\Redirect;
+namespace App\Http\Controllers;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return Inertia::render('List', ['categories' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Form');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required',
+                'slug' => 'required',
+            ]
+        );
+
+        Category::create($request->all());
+
+        return Redirect::route('category.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $categories = Category::find($id);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return Inertia::render('EditForm', ['id' => $id]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $categories = Category::find($id);
+        $categories->update($request->all());
+        return Redirect::route('category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $categories = Category::find($id);
+        $categories->delete();
+        return Redirect::route('category.index');
+    }
+}
